54b7d662833287bb5c78b392fd4a4144eed8e7ab
nalyk <dev.ungheni@gmail.com>
2018-11-02 13:16:23 +0200

    update

---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,23 @@
+﻿// Gruntfile.js
+module.exports = grunt => {
+        // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+        require('load-grunt-tasks')(grunt);
+
+        grunt.initConfig({
+        copy: {
+            main: {
+                files: [
+                        // jQuery
+                        {expand: true, cwd: 'node_modules/jquery/dist', src: ['**'], dest: '../../public/js/', filter: 'isFile'},
+
+                        // popper.js
+                        {expand: true, cwd: 'node_modules/popper.js/dist/umd', src: ['**'], dest: '../../public/js/', filter: 'isFile'},
+
+                        // Bootstrap
+                        {expand: true, cwd: 'node_modules/bootstrap/dist/css', src: ['**'], dest: '../../public/css/', filter: 'isFile'},
+                        {expand: true, cwd: 'node_modules/bootstrap/dist/js', src: ['**'], dest: '../../public/js/', filter: 'isFile'},
+                ]
+            }
+        },
+    });
+};
