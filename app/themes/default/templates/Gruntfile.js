60956035628a62967ef7408fc0db33e60604eefd
nalyk <dev.ungheni@gmail.com>
2018-11-03 13:11:01 +0200

    Template tune

--- app/themes/default/templates/Gruntfile.js
+++ app/themes/default/templates/Gruntfile.js
@@ -8,14 +8,14 @@ module.exports = grunt => {
             main: {
                 files: [
                         // jQuery
-                        {expand: true, cwd: 'node_modules/jquery/dist', src: ['**'], dest: '../../public/js/', filter: 'isFile'},
+                        {expand: true, cwd: 'node_modules/jquery/dist', src: ['**'], dest: '../public/js/', filter: 'isFile'},
 
                         // popper.js
-                        {expand: true, cwd: 'node_modules/popper.js/dist/umd', src: ['**'], dest: '../../public/js/', filter: 'isFile'},
+                        {expand: true, cwd: 'node_modules/popper.js/dist/umd', src: ['**'], dest: '../public/js/', filter: 'isFile'},
 
                         // Bootstrap
-                        {expand: true, cwd: 'node_modules/bootstrap/dist/css', src: ['**'], dest: '../../public/css/', filter: 'isFile'},
-                        {expand: true, cwd: 'node_modules/bootstrap/dist/js', src: ['**'], dest: '../../public/js/', filter: 'isFile'},
+                        {expand: true, cwd: 'node_modules/bootstrap/dist/css', src: ['**'], dest: '../public/css/', filter: 'isFile'},
+                        {expand: true, cwd: 'node_modules/bootstrap/dist/js', src: ['**'], dest: '../public/js/', filter: 'isFile'},
                 ]
             }
         },
